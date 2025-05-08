a3c525dea4dcace60585dbda98b348080073a2e0
Ansh Yadav <ansh@zocket.com>
2025-05-08 22:53:30 +0530

    cors error

--- main.go
+++ main.go
@@ -4,11 +4,18 @@ import (
 	config "report/base"
 	"report/router"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, 
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
 
 	router.IntiateRoutes(r)
 	config.LoadConfig()
